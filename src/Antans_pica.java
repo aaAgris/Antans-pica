a240eae3540ed35e900827df4f5775f16aea00ed
antans <antanswork@gmail.com>
2022-01-26 19:11:28 +0200

    Tiek veidota iespēja izvēlēties no Pizzo kataloga

--- src/Antans_pica.java
+++ src/Antans_pica.java
@@ -1,8 +1,42 @@
+import javax.swing.JOptionPane;
 
 public class Antans_pica {
+	
+	
+	public static void cenas() {
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "__Pizzo-Picu katalogs__","Pizzo",JOptionPane.PLAIN_MESSAGE);
+		String izvele;
+		do {
+			 izvele = JOptionPane.showInputDialog("Darbības ar Pizzo katalogu\n"
+					+ "1-Pievienot klienta pasūtījumu\n"
+					+ "2-Pizzo cenu apskate\n"
+					+ "STOP-apturēt darbības ar katalogu");
+			 izvele = izvele.toUpperCase();
+			 
+			 switch(izvele) {
+			 
+			 case "1":
+				 
+			break;
+			
+			 case "2":
+				 cenas();
+			break;
+			
+			 case "STOP":
+				 JOptionPane.showMessageDialog(null, "Pizzo - izslēdzas...","Pizzo", JOptionPane.INFORMATION_MESSAGE);
+			break;
+			
+			default:JOptionPane.showMessageDialog(null, "Pizzo darbībā radusies kļūda","Pizzo", JOptionPane.WARNING_MESSAGE);
+			 
+			 }
+			 
+			 
+		}while(!izvele.equals("STOP"));
 
 	}
 
